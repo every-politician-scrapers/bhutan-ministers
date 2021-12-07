97d304945ee97946cf8cfe8bfd49e3b05a4ac922
Tony Bowden <tony@tmtm.com>
2021-12-07 11:35:02 +0200

    initial scraper

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -7,17 +7,31 @@ require 'pry'
 class MemberList
   class Member
     def name
-      noko.css('.name').text.tidy
+      textparts[0].to_s.delete_prefix('Lyonpo ')
     end
 
     def position
-      noko.css('.position').text.tidy
+      textparts[1].to_s.sub('Ministry', 'Minister')
+    end
+
+    def empty?
+      name.empty?
+    end
+
+    private
+
+    def textparts
+      noko.xpath('.//text()').map(&:text).map(&:tidy).reject(&:empty?)
     end
   end
 
   class Members
+    def member_items
+      super.reject(&:empty?)
+    end
+
     def member_container
-      noko.css('.member')
+      noko.css('#wp-table-reloaded-id-3-no-1 td')
     end
   end
 end
